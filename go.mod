18a4a2d5828bdcd4f14fd97665a29787750aef50
TZ <zsco@users.noreply.github.com>
2025-02-27 13:56:35 +0100

    replace mux router with gin

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/SENERGY-Platform/analytics-flow-engine
 
-go 1.23
+go 1.23.0
 
 toolchain go1.23.4
 
@@ -11,48 +11,71 @@ require (
 	github.com/SENERGY-Platform/go-service-base/watchdog v0.4.1
 	github.com/SENERGY-Platform/models/go v0.0.0-20240627082833-157bd627a94f
 	github.com/SENERGY-Platform/permissions-v2 v0.0.27
+	github.com/SENERGY-Platform/service-commons v0.0.0-20250123095636-6dfc659ee43e
 	github.com/eclipse/paho.mqtt.golang v1.4.3
+	github.com/gin-contrib/cors v1.7.3
+	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.5.1
 	github.com/parnurzeal/gorequest v0.2.16
 	github.com/pkg/errors v0.9.1
-	github.com/rs/cors v1.10.1
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/SENERGY-Platform/developer-notifications v0.0.4 // indirect
-	github.com/SENERGY-Platform/service-commons v0.0.0-20240813072046-91b3195dd8fc // indirect
+	github.com/bytedance/sonic v1.12.9 // indirect
+	github.com/bytedance/sonic/loader v0.2.3 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/elazarl/goproxy v0.0.0-20231117061959-7cc037d33fb5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gin-contrib/sse v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
 	github.com/go-openapi/spec v0.20.6 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.25.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/segmentio/kafka-go v0.4.47 // indirect
 	github.com/smartystreets/goconvey v1.8.1 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
 	github.com/swaggo/http-swagger v1.3.4 // indirect
 	github.com/swaggo/swag v1.16.3 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.mongodb.org/mongo-driver v1.15.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/arch v0.14.0 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.19.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	moul.io/http2curl v1.0.0 // indirect
 )
--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -17,18 +17,22 @@
 package api
 
 import (
+	"fmt"
 	devicemanager_api "github.com/SENERGY-Platform/analytics-flow-engine/pkg/device-manager-api"
 	kafka2mqtt_api "github.com/SENERGY-Platform/analytics-flow-engine/pkg/kafka2mqtt-api"
 	"github.com/SENERGY-Platform/analytics-flow-engine/pkg/lib"
 	"github.com/SENERGY-Platform/analytics-flow-engine/pkg/parsing-api"
 	permission_api "github.com/SENERGY-Platform/analytics-flow-engine/pkg/permission-api"
 	rancher2_api "github.com/SENERGY-Platform/analytics-flow-engine/pkg/rancher2-api"
+	"github.com/SENERGY-Platform/service-commons/pkg/jwt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"slices"
+	"strconv"
+	"strings"
 
 	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 )
 
 func CreateServer() {
@@ -48,25 +52,129 @@ func CreateServer() {
 	permission := permission_api.NewPermissionApi(lib.GetEnv("PERMISSION_API_ENDPOINT", ""))
 	kafka2mqtt := kafka2mqtt_api.NewKafka2MqttApi(lib.GetEnv("KAFKA2MQTT_API_ENDPOINT", ""))
 	deviceManager := devicemanager_api.NewDeviceManagerApi(lib.GetEnv("DEVICE_MANAGER_API_ENDPOINT", ""))
+	flowEngine := lib.NewFlowEngine(driver, parser, permission, kafka2mqtt, deviceManager)
 
 	port := lib.GetEnv("API_PORT", "8000")
 	log.Println("Starting Server at port " + port + "\n")
-	router := mux.NewRouter()
-
-	e := NewEndpoint(driver, parser, permission, kafka2mqtt, deviceManager)
-	router.HandleFunc("/", e.getRootEndpoint).Methods("GET")
-	router.HandleFunc("/pipeline/{id}", e.getPipelineStatus).Methods("GET")
-	router.HandleFunc("/pipelines", e.getPipelinesStatus).Methods("POST")
-	router.HandleFunc("/pipeline", e.startPipeline).Methods("POST")
-	router.HandleFunc("/pipeline", e.updatePipeline).Methods("PUT")
-	router.HandleFunc("/pipeline/{id}", e.deletePipeline).Methods("DELETE")
-	c := cors.New(
-		cors.Options{
-			AllowedHeaders: []string{"Content-Type", "Authorization"},
-			AllowedOrigins: []string{"*"},
-			AllowedMethods: []string{"GET", "POST", "DELETE", "OPTIONS", "PUT"},
+	DEBUG, err := strconv.ParseBool(lib.GetEnv("DEBUG", "false"))
+	if err != nil {
+		log.Print("Error loading debug value")
+		DEBUG = false
+	}
+	if !DEBUG {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "OPTIONS", "PUT"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	prefix := r.Group(lib.GetEnv("ROUTE_PREFIX", ""))
+	prefix.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
 		})
-	handler := c.Handler(router)
-	logger := lib.NewLogger(handler, "CALL")
-	log.Fatal(http.ListenAndServe(lib.GetEnv("SERVERNAME", "")+":"+port, logger))
+	})
+	prefix.GET("/pipeline/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		pipelineStatus, err := flowEngine.GetPipelineStatus(id, getUserId(c), c.GetHeader("Authorization"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		c.JSON(http.StatusOK, pipelineStatus)
+	})
+	prefix.POST("/pipelines", func(c *gin.Context) {
+		var request lib.PipelineStatusRequest
+		if err := c.ShouldBindJSON(&request); err != nil {
+			log.Println(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		pipelinesStatus, err := flowEngine.GetPipelinesStatus(request.Ids, getUserId(c), c.GetHeader("Authorization"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		c.JSON(http.StatusOK, pipelinesStatus)
+	})
+
+	prefix.POST("/pipeline", func(c *gin.Context) {
+		var request lib.PipelineRequest
+		if err := c.ShouldBindJSON(&request); err != nil {
+			log.Println(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		pipe, err := flowEngine.StartPipeline(request, getUserId(c), c.GetHeader("Authorization"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		c.JSON(http.StatusOK, pipe)
+	})
+
+	prefix.PUT("/pipeline", func(c *gin.Context) {
+		var request lib.PipelineRequest
+		if err := c.ShouldBindJSON(&request); err != nil {
+			log.Println(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		pipe, err := flowEngine.UpdatePipeline(request, getUserId(c), c.GetHeader("Authorization"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		c.JSON(http.StatusOK, pipe)
+	})
+
+	prefix.DELETE("/pipeline/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		err := flowEngine.DeletePipeline(id, getUserId(c), c.GetHeader("Authorization"))
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.Status(http.StatusNoContent)
+	})
+
+	if !DEBUG {
+		err = r.Run(":" + port)
+	} else {
+		err = r.Run("127.0.0.1:" + port)
+	}
+	if err == nil {
+		fmt.Printf("Starting api server failed: %s \n", err)
+	}
+}
+
+func getUserId(c *gin.Context) (userId string) {
+	forUser := c.Query("for_user")
+	if forUser != "" {
+
+		roles := strings.Split(c.GetHeader("X-User-Roles"), ", ")
+		if slices.Contains[[]string](roles, "admin") {
+			return forUser
+		}
+	}
+
+	userId = c.GetHeader("X-UserId")
+	if userId == "" {
+		if c.GetHeader("Authorization") != "" {
+			claims, err := jwt.Parse(c.GetHeader("Authorization"))
+			if err != nil {
+				return
+			}
+			userId = claims.Sub
+			if userId == "" {
+				userId = "dummy"
+			}
+		}
+	}
+	return
 }
