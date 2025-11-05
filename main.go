c33e0a2065f09565075366405e8f9d7a9f2779ac
TZ <zsco@users.noreply.github.com>
2025-11-05 15:16:00 +0100

    fix logging org

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ import (
 	sb_util "github.com/SENERGY-Platform/go-service-base/util"
 )
 
-var Version = "0.0.59"
+var Version = "0.0.60"
 
 func main() {
 	ec := 0
--- pkg/util/logger.go
+++ pkg/util/logger.go
@@ -31,8 +31,8 @@ var Logger *slog.Logger
 func InitStructLogger(level string) {
 	if Logger == nil {
 		info, ok := debug.ReadBuildInfo()
-		project := "github.com/SENERGY-Platform"
-		org := "analytics-flow-engine"
+		project := "analytics-flow-engine"
+		org := "github.com/SENERGY-Platform"
 		if ok {
 			if parts := strings.Split(info.Main.Path, "/"); len(parts) > 2 {
 				project = strings.Join(parts[2:], "/")
